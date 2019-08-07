e0f542a7b1035a17b0e001a614ef7861aacfc760
Tamás Vasadi-Balogh <vasadibt@gmail.com>
2019-08-07 18:19:56 +0200

    Add new entities and managers, and add connector dependency config

---
+++ src/TestOneBundle/Services/InvoiceManager.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\TestOneBundle\Services;
+
+
+use App\TestOneBundle\Entities\Invoice;
+use App\TestOneBundle\Entities\Product;
+use App\TestOneBundle\Helpers\Faker;
+use App\TestOneBundle\Interfaces\ManagerInterface;
+use App\TestOneBundle\Interfaces\UrlizeInterface;
+use App\TestOneBundle\Traits\ManagerTrait;
+
+class InvoiceManager implements ManagerInterface, UrlizeInterface
+{
+    use ManagerTrait;
+
+    const URL = '/finance';
+
+    /**
+     * @return Product[]
+     * @throws \Exception
+     */
+    public function findAll(): array
+    {
+        $entities = [];
+        for ($i = 0; $i < 3; $i++) {
+            $entities [] = new Invoice(Faker::email(), rand(1, 5), Faker::productDescription());
+        }
+        return $entities;
+    }
+}

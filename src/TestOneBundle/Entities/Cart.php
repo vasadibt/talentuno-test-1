7d5db522d85668c42ca6478ad690c769f83eb198
Tamás Vasadi-Balogh <vasadibt@gmail.com>
2019-08-07 14:26:12 +0200

    Build Entities and add Validator

---
+++ src/TestOneBundle/Entities/Cart.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\TestOneBundle\Entities;
+
+use App\TestOneBundle\Interfaces\EntityInterface;
+use App\TestOneBundle\Utils\Validator;
+
+/**
+ * Class Cart
+ * @package App\TestOneBundle\Entities
+ */
+class Cart implements EntityInterface
+{
+    /**
+     * Cart item attributes
+     */
+    const ITEM_ATTRIBUTES = [
+        'productId',
+        'quantity',
+    ];
+
+    /**
+     * @var string
+     */
+    protected $email;
+    /**
+     * @var array[]
+     * - productId
+     * - quantity
+     */
+    protected $items = [];
+
+    /**
+     * Cart constructor.
+     * @param string|null $email
+     * @param array $items
+     * @throws \Exception
+     */
+    public function __construct(string $email = null, array $items = [])
+    {
+        if ($email !== null) {
+            $this->setEmail($email);
+        }
+        if ($items !== []) {
+            $this->setItems($items);
+        }
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @throws \Exception
+     */
+    public function setEmail(string $email): void
+    {
+        if (!Validator::validateEmail($email)) {
+            throw new \Exception("The 'email' param is not valid!");
+        }
+        $this->email = $email;
+    }
+
+    /**
+     * @return array[]
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param array[] $items
+     * @throws \Exception
+     */
+    public function setItems(array $items): void
+    {
+        foreach ($items as $item) {
+            if (!Validator::validateArrayKeys($item, static::ITEM_ATTRIBUTES)) {
+                throw new \Exception('The "item" is not valid!');
+            }
+        }
+        $this->items = $items;
+    }
+}
---
+++ src/TestOneBundle/Entities/Product.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\TestOneBundle\Entities;
+
+use App\TestOneBundle\Interfaces\EntityInterface;
+
+/**
+ * Class Product
+ * @package App\TestOneBundle\Entities
+ */
+class Product implements EntityInterface
+{
+    /**
+     * @var mixed
+     */
+    protected $productId;
+    /**
+     * @var float
+     */
+    protected $price;
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * Product constructor.
+     *
+     * @param mixed|null $productId
+     * @param float|null $price
+     * @param string|null $description
+     */
+    public function __construct($productId = null, float $price = null, string $description = null)
+    {
+        if ($productId !== null) {
+            $this->setProductId($productId);
+        }
+        if ($price !== null) {
+            $this->setProductId($price);
+        }
+        if ($description !== null) {
+            $this->setDescription($price);
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getProductId()
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param mixed $productId
+     */
+    public function setProductId($productId): void
+    {
+        $this->productId = $productId;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+}
